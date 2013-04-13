492f5cafa1172fa7a9b67a38ca3d747fd21e7146
Ivan Vergiliev <ivan.vergiliev@gmail.com>
2013-04-13 23:26:07 +0300

    Load the course description modal contents dynamically.

--- static/js/user-courses.js
+++ static/js/user-courses.js
@@ -10,3 +10,10 @@ $(function() {
     $('#firstReCAPTCHA').html($('#originalReCAPTCHA').clone(true, true));
     $('#secondReCAPTCHA').html($('#originalReCAPTCHA').clone(true, true));
 });
+
+$('.showDescription').click(function() {
+  $.get('templates/course_info.ejs', function (txt) {
+    $('.modal').html(txt);
+    $('#info-linux-sys-admin').modal('show');
+  });
+});
