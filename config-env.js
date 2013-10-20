74884302871154c2dfed8f2cbdae6a5f2e92f057
Ivan Vergiliev <ivan.vergiliev@gmail.com>
2013-10-20 18:19:44 +0300

    Use MongoLab on Heroku.

--- config-env.js
+++ config-env.js
@@ -1,5 +1,5 @@
 module.exports = {
-  dbString: 'mongodb://localhost/hackfmi',
+  dbString: process.env.MONGOLAB_URI || 'mongodb://localhost/hackfmi',
   recaptchaPublic: process.env.RECAPTCHA_PUBLIC, // ReCaptcha public key - must register at the ReCaptche Developers site
   recaptchaPrivate: process.env.RECAPTCHA_PRIVATE // ReCaptcha private key - do not publish online.
 };
