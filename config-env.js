fa03dac2852df4391d542d34c943a6b2e8817b65
Ivan Vergiliev <ivan.vergiliev@gmail.com>
2013-10-20 18:23:28 +0300

    Listen to PORT on heroku

--- config-env.js
+++ config-env.js
@@ -1,4 +1,5 @@
 module.exports = {
+  port: process.env.PORT,
   dbString: process.env.MONGOLAB_URI || 'mongodb://localhost/hackfmi',
   recaptchaPublic: process.env.RECAPTCHA_PUBLIC, // ReCaptcha public key - must register at the ReCaptche Developers site
   recaptchaPrivate: process.env.RECAPTCHA_PRIVATE // ReCaptcha private key - do not publish online.
